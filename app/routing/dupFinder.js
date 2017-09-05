895cdc452ed8abac7550aa225aca589c02012fc6
Vit Benton <vitbenton@Macintosh.local>
2017-09-04 23:36:20 -0400

    completed basic user dup check

--- app/routing/dupFinder.js
+++ app/routing/dupFinder.js
@@ -2,8 +2,14 @@ var friendsData = require("../data/friends");
 
 var dupFinder = function (user){
 
-	
+	var dupFound = false;
 
+	for (i = 0; i < friendsData.length; i++) {
+		if (user.name === friendsData[i].name && user.photo === friendsData[i].photo)
+			dupFound = true;
+	}
+
+	return dupFound;
 }
 
 
